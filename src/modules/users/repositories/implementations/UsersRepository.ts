c81ea67a6d874ed16cba2924a3f54f2a16a759a1
Phablo Vilas Boas <phablo_sena@hotmail.com>
2022-03-15 06:10:00 -0300

    ginished

---
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -0,0 +1,52 @@
+import { getRepository, Repository } from 'typeorm';
+
+import { IFindUserWithGamesDTO, IFindUserByFullNameDTO } from '../../dtos';
+import { User } from '../../entities/User';
+import { IUsersRepository } from '../IUsersRepository';
+
+export class UsersRepository implements IUsersRepository {
+  private repository: Repository<User>;
+
+  constructor() {
+    this.repository = getRepository(User);
+  }
+
+  async findUserWithGamesById({
+    user_id,
+  }: IFindUserWithGamesDTO): Promise<User> {
+    // const [user, ] = await this.repository.find({ 
+    //   where: {id: user_id}, 
+    //   relations: ["games"]
+    // });
+
+    const user = await this.repository.findOne(
+      user_id, 
+      { relations: ["games"] }
+    );
+
+    if (!user) {
+      throw new Error("User does not exist!");
+    }
+
+    return user;
+  }
+
+  async findAllUsersOrderedByFirstName(): Promise<User[]> {
+    return await this.repository.query(`
+      SELECT * 
+      FROM users
+      ORDER BY first_name ASC
+    `); // Complete usando raw query
+  }
+
+  async findUserByFullName({
+    first_name,
+    last_name,
+  }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
+    return await this.repository.query(`
+      SELECT * 
+      FROM users
+      WHERE LOWER(first_name) = LOWER('${first_name}') AND LOWER(last_name) = LOWER('${last_name}')
+    `); // Complete usando raw query
+  }
+}
